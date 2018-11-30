990c35b29fdadc7538bbfb8cf3558c4861945956
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-11-30 16:01:30 +0500

    Users module along with validation using validator library

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://zainkhan2504:zainkhan2504@ds231739.mlab.com:31739/devconnector"
+    "mongodb://zainkhan2504:zainkhan2504@ds231739.mlab.com:31739/devconnector",
+  secretOrKey: "secret"
 };
