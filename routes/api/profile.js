c7a8aef8f2478cc1846a331b77a90f90e2ce0b74
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-12-01 17:19:26 +0500

    Added Post Module

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -258,10 +258,10 @@ router.delete(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
-      User.findOneAndRemove({ _id: req.user.id }).then(() =>
-        res.json({ success: true })
-      );
+    Profile.findOneAndDelete({ user: req.user.id }).then(() => {
+      User.findOneAndDelete({
+        _id: req.user.id
+      }).then(() => res.json({ success: true }));
     });
   }
 );
