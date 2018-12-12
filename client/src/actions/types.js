e22172bd3d36d4ff71c0ad6b7ab7a8b0d42f8795
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-12-12 19:04:47 +0500

    Create post, comment and like post

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,16 @@
+// For User
 export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+// For Profile
 export const GET_PROFILE = "GET_PROFILE";
 export const PROFILE_LOADING = "PROFILE_LOADING";
 export const PROFILE_NOT_FOUND = "PROFILE_NOT_FOUND";
 export const CLEAR_CURRENT_PROFILE = "CLEAR_CURRENT_PROFILE";
 export const GET_PROFILES = "GET_PROFILES";
+// For Post
+export const POST_LOADING = "POST_LOADING";
+export const GET_POSTS = "GET_POSTS";
+export const GET_POST = "GET_POST";
+export const ADD_POST = "ADD_POST";
+export const DELETE_POST = "DELETE_POST";
